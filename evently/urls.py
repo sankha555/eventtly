ff88c16d453c09e2b2ae2450b2d9ef6346a7d7a5
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2020-06-08 00:27:10 +0530

    Facebook login

--- evently/urls.py
+++ evently/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from events.views import prevent_multiple_login
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
